1c5cd46485a6d4bbd4da8bb78e26eedb398197f4
Jorge Luis Silvério de Lima <jorgezarpon@msn.com>
2026-01-18 09:36:19 -0300

    Update version and sysctl parameters in InstallTD.sh

--- InstallTD.sh
+++ InstallTD.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # --- versão e autor do script ---
 
-versao="1.7.7- ENDLESS GAME"
+versao="1.7.7. Rev01- ENDLESS GAME"
 autor="Jorge Luis"
 pix_doacao="jorgezarpon@msn.com"
 
@@ -25,8 +25,8 @@ readonly dxvk_cache_path="/home/deck/dxvkcache"
 readonly base_sysctl_params=(
     
     "vm.vfs_cache_pressure=50"           # Reduzido de 66: Mantém cache de diretórios na RAM por mais tempo
-    "vm.dirty_background_bytes=67108864"   
-    "vm.dirty_bytes=402653184"              
+    "vm.dirty_background_ratio=1" 
+    "vm.dirty_ratio=4"            
     "vm.dirty_expire_centisecs=4500"       
     "vm.dirty_writeback_centisecs=1500"     
     "vm.min_free_kbytes=131072"
