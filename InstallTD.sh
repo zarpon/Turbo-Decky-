3e7a082c86aa9eeca9b9941145c8d9ebf4a0a787
Jorge Luis Silvério de Lima <jorgezarpon@msn.com>
2026-01-21 14:28:38 -0300

    Update script version to 1.7.7 Rev05

--- InstallTD.sh
+++ InstallTD.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # --- versão e autor do script ---
 
-versao="1.7.7. Rev04- ENDLESS GAME"
+versao="1.7.7. Rev05- ENDLESS GAME"
 autor="Jorge Luis"
 pix_doacao="jorgezarpon@msn.com"
 
