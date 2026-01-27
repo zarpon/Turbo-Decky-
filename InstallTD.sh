bb633656c1a79dbd52f98d317d60ca383f4e02a3
Jorge Luis Silvério de Lima <jorgezarpon@msn.com>
2026-01-27 17:26:09 -0300

    Update script version and sysctl parameters
    Updated version to 1.8 Rev04 and modified sysctl parameters for swappiness and vfs_cache_pressure.

--- InstallTD.sh
+++ InstallTD.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # --- versão e autor do script ---
 
-versao="1.8 Rev03"
+versao="1.8 Rev04"
 autor="Jorge Luis"
 pix_doacao="jorgezarpon@msn.com"
 
@@ -1045,7 +1045,7 @@ echo 1 > /sys/module/zswap/parameters/shrinker_enabled 2>/dev/null || true
 echo 1 > /sys/kernel/mm/page_idle/enable 2>/dev/null || true
 sysctl -w vm.fault_around_bytes=32 2>/dev/null || true
 sysctl -w vm.swappiness=80 || true
-sysctl -w vm.vfs_cache_pressure=66 || true
+sysctl -w vm.vfs_cache_pressure=110 || true
 ZSWAP_SCRIPT
     chmod +x /usr/local/bin/zswap-config.sh
 
@@ -1168,8 +1168,8 @@ fi
 
 
 echo 1 > /sys/kernel/mm/page_idle/enable 2>/dev/null || true
-sysctl -w vm.swappiness=133 || true
-sysctl -w vm.vfs_cache_pressure=66  || true
+sysctl -w vm.swappiness=180 || true
+sysctl -w vm.vfs_cache_pressure=150  || true
 sysctl -w vm.fault_around_bytes=32 2>/dev/null || true
 echo "=== ZRAM STATUS ===" >> /var/log/turbodecky.log
 zramctl >> /var/log/turbodecky.log
