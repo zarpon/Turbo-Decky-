7cb3a856e537305a78a8e706fc1bee0cccfa7edb
Jorge Luis Silvério de Lima <jorgezarpon@msn.com>
2026-02-18 16:03:32 -0300

    Add files via upload

--- InstallTD.sh
+++ InstallTD.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # --- versão e autor do script ---
 
-versao="2.5.rev12 - Timeless Child"
+versao="2.5.rev13 - Timeless Child"
 autor="Jorge Luis"
 pix_doacao="jorgezarpon@msn.com"
 
@@ -30,10 +30,10 @@ readonly dxvk_cache_path="/home/deck/dxvkcache"
 
 # --- parâmetros sysctl base (ATUALIZADO PARA LATÊNCIA E SCHEDULER) ---
 readonly base_sysctl_params=(
-    "vm.dirty_background_bytes=264857600"
-    "vm.dirty_bytes=1073741824"
-    "vm.dirty_expire_centisecs=3500"       
-    "vm.dirty_writeback_centisecs=1500"     
+    "vm.dirty_background_bytes=164857600"
+    "vm.dirty_bytes=3073741824"
+    "vm.dirty_expire_centisecs=4500"       
+    "vm.dirty_writeback_centisecs=500"     
     "vm.page-cluster=0" 
     "vm.page_lock_unfairness=1"
     "vm.compaction_proactiveness=8"
