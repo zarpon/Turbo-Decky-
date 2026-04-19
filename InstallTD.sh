6b92545d0fe037ba5ca928e63e47129c488c6d2c
Jorge Luis Silvério de Lima <jorgezarpon@msn.com>
2026-04-19 14:35:33 -0300

    Update InstallTD.sh

--- InstallTD.sh
+++ InstallTD.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # --- versão e autor do script ---
 
-versao="3.2.8 19-04 R1 - - Timeless Child"
+versao="3.2.8 19-04 - - Timeless Child"
 autor="Jorge Luis"
 pix_doacao="jorgezarpon@msn.com"
 
