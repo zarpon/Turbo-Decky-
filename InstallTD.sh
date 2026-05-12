dfb348d64c76393e10ea7ee520202089e912363c
Jorge Luis Silvério de Lima <jorgezarpon@msn.com>
2026-05-12 17:16:51 -0300

    Update script version to 3.5 - 12-05

--- InstallTD.sh
+++ InstallTD.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # --- versão e autor do script ---
 
-versao="3.5 - 07-05 R5 - Timeless Child"
+versao="3.5 - 12-05 - Timeless Child"
 autor="Jorge Luis"
 pix_doacao="jorgezarpon@msn.com"
 
@@ -49,6 +49,7 @@ readonly unnecessary_services=(
 # --- variáveis de ambiente (Configuração de Jogos) ---
 # Nota: DXVK_STATE_CACHE_PATH usa a variável definida acima
 readonly game_env_vars=(
+    "RADV_PERFTEST=aco"
     "MESA_SHADER_CACHE_MAX_SIZE=10G"
      "MESA_DISK_CACHE_DATABASE=1"    
 )  
